db9716ee3c2bb8e25f6c1f88b236e863d8fe6b81
Roman Kovalev <epoxxid@gmail.com>
2019-02-09 18:23:05 +0300

    Fixed js-test message

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,7 @@ document.addEventListener('click', (e) => {
 	if (e.target.tagName === 'BUTTON') {
 		const h1 = document.getElementsByTagName('h1')[0];
 		const oldText = h1.textContent;
-		h1.textContent = 'Ты самая лучшая жена!';
+		h1.textContent = 'JavaScript works too!';
 		setTimeout(() => {
 			h1.textContent = oldText;
 		}, 2000)
